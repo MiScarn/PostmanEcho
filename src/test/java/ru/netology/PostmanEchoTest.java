38d59373a4c3cd2b9ca5eb330bc93b73790a3a60
MiScarn <bringmenoize@gmail.com>
2023-12-06 01:15:08 -0500

    Fixed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Привет")
+                .body("Привет, МАЙ")
                 .when()
                 .post("/post")
                 // Проверки
